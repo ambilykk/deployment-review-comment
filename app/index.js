e579ee3660372828c3c21da5b769dc2fe9db79cd
Ambily <ambilykk@Ambilys-MacBook-Pro.local>
2022-12-28 11:32:42 +0530

    added the async function

--- app/index.js
+++ app/index.js
@@ -7,25 +7,31 @@ const run_id = core.getInput('run-id');
 // if run_id is not provided, use the current run id
 run_id = run_id ? run_id : github.context.runId;
 
-// define output array
-const commentsRes = [];
+// define the run function
+async function run() {
+    // define output array
+    const commentsRes = [];
 
-// get the Review data for the run
-const run_data = await github.rest.actions.getReviewsForRun({
-    owner: context.repo.owner,
-    repo: context.repo.repo,
-    run_id: run_id
-})
+    // get the Review data for the run
+    const run_data = await github.rest.actions.getReviewsForRun({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        run_id: run_id
+    })
 
-// for multiple comments
-for (const data of run_data.data) {
-    console.log(data.comment);
-    commentsRes.push(data);
+    // for multiple comments
+    for (const data of run_data.data) {
+        console.log(data.comment);
+        commentsRes.push(data);
+    }
+
+    // set the output value
+    core.setOutput(comments, commentsRes);
+    console.log(commentsRes);
+
+    // console the completion message
+    console.log("Completed");
 }
 
-// set the output value
-core.setOutput(comments, commentsRes);
-console.log(commentsRes);
-
-// console the completion message
-console.log("Completed");
+// executing the run function
+run();
